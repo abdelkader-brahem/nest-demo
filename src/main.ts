b27f3e050758f5c29ff03ae06f3974ade65df5c5
abdelkader brahem <abdelkader.brahem@comwork.io>
2022-01-22 18:28:40 +0100

    improve profile picture

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import hbs = require('hbs');
+import express = require('express');
 
 /*****
  * Author : Brahem Abdelkader
@@ -12,7 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-  hbs.registerPartials(join(__dirname, '..', 'views','images'));
+  app.use('/images', express.static(join(__dirname, '..', 'images'))); // <-
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
